34bcf3cbacdbed408a5bcfff6908a9ebd76f4460
DanHembery <93157414+DanHembery@users.noreply.github.com>
2021-11-28 11:19:14 +0000

    arrays completed

--- src/arrays.js
+++ src/arrays.js
@@ -1,61 +1,76 @@
 const getNthElement = (index, array) => {
-  // your code here
+  m = (array.length);
+  if(index>0){
+    index = index % m;
+  };
+  return array[index];
 };
 
 const arrayToCSVString = array => {
-  // your code here
+  return array.toString();
 };
 
 const csvStringToArray = string => {
-  // your code here
+  return string.split(',');
 };
 
 const addToArray = (element, array) => {
-  // your code here
+  array.push(element);
 };
 
 const addToArray2 = (element, array) => {
-  // your code here
+ 
+  return array.concat(element);
 };
 
 const removeNthElement = (index, array) => {
-  // your code here
+  return array.splice(index,1);
 };
 
 const numbersToStrings = numbers => {
-  // your code here
+  return numbers.map(String);
 };
 
 const uppercaseWordsInArray = strings => {
-  // your code here
-};
+  return strings.map(f => f.toUpperCase());
+  };
 
 const reverseWordsInArray = strings => {
-  // your code here
+  return strings.map(f => f
+    .split("")
+    .reverse()
+    .join(""))
 };
 
 const onlyEven = numbers => {
-  // your code here
+  return numbers.filter(f => f % 2 === 0);
 };
 
 const removeNthElement2 = (index, array) => {
-  // your code here
+  let arrayNew = array.slice();
+  arrayNew.splice(index, 1);
+  return arrayNew;
 };
 
 const elementsStartingWithAVowel = strings => {
-  // your code here
+  let vowelCheck = /^[aeiouAEIOU]/i;
+  let newArray = strings.filter(strings => vowelCheck.test(strings));
+  return newArray;
 };
 
 const removeSpaces = string => {
-  // your code here
+  let noSpace = string.replace(/ /g, "");
+  return noSpace;
 };
 
 const sumNumbers = numbers => {
-  // your code here
-};
+  return numbers.reduce((a, c) =>
+    a + c, 0);
+  };
 
 const sortByLastLetter = strings => {
-  // your code here
+  return strings.sort((a, b) => a.charCodeAt(a.length - 1)
+  - b.charCodeAt(b.length - 1));  
 };
 
 module.exports = {
--- src/objects.js
+++ src/objects.js
@@ -1,25 +1,27 @@
 const createPerson = (name, age) => {
-  // your code here
+  return {name, age};
 };
 
 const getName = object => {
-  // your code here
+  return object.name;
 };
 
 const getProperty = (property, object) => {
-  // your code here
+  return object[property];
 };
 
 const hasProperty = (property, object) => {
-  // your code here
+  if (object[property]) return true;
+  return false;
 };
 
 const isOver65 = person => {
-  // your code here
+  if (person.age > 65) return true;
+  return false;
 };
 
 const getAges = people => {
-  // your code here
+  return people.map(newArray => newArray.age);
 };
 
 const findByName = (name, people) => {
